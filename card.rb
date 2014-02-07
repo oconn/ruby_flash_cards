0fdb34c213447e697825011cf491c6ae6bfecde0
Apprentice <apprentice@dbc-chicago-019.local>
2014-02-07 11:20:13 -0600

    change csv file

--- card.rb
+++ card.rb
@@ -1,5 +1,9 @@
+require 'csv'
+
+
 class Card
-  attr_reader :definition, :answer,:id, :used
+  attr_accessor :used
+  attr_reader :definition, :answer,:id
   def initialize(args)
     @id = args[:id]
     @definition = args[:definition]
@@ -12,27 +16,30 @@ class Card
   end
 
   def to_s
-    "\033[36m#{question}\033[0m - \033[5m\033[1m\033[32m#{answer}\033[0m"
+    "\033[36m#{definition}\033[0m - \033[5m\033[1m\033[32m#{answer}\033[0m"
   end
 end
 
 class Deck
   attr_reader :csv_file, :contents
-  def initialize(csv_file)
-    @csv_file = csv_file
+  def initialize#(csv_file)
+    #@csv_file = csv_file
     @contents = []
   end
 
   def populate_contents
-    csv_file.each{|args| contents << add_card(args)}
+    CSV.foreach('flashcard.csv', headers: true, :header_converters => :symbol) do |row|
+      add_card(row)
+    end
   end
 
   def change_card_status(card)
     card.has_been_used
+    card
   end
 
   def new_card
-    card = contents.select{|card| card.used == false}.sample(0)
+    card = contents.select{|card| card.used == false}.sample
     change_card_status(card)
   end
 
@@ -46,17 +53,17 @@ class Deck
 end
 
 # a bit of testing
-questions = [ {question: "What is your name", answer: "Alex" ,id: "1" },
-              {question: "What is your age", answer: "27" ,id: "2"},
-              {question: "What color is an orange", answer: "orange" ,id: "3"}]
+questions = [ {definition: "What is your name", answer: "Alex" ,id: "1" },
+              {definition: "What is your age", answer: "27" ,id: "2"},
+              {definition: "What color is an orange", answer: "orange" ,id: "3"}]
 
-my_deck = Deck.new(questions)
+my_deck = Deck.new
 
 my_deck.populate_contents
 
-p my_deck.contents[1].question
+p my_deck.contents[1].definition
 p my_deck.contents[1].answer
-puts my_deck.new_card.used
+puts my_deck.new_card
 
 
 
