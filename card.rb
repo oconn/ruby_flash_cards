1f0102d47823b4c61f0a8bd18c9ddc1672175c83
Apprentice <apprentice@dbc-chicago-019.local>
2014-02-07 14:11:34 -0600

    added skip to the card

--- card.rb
+++ card.rb
@@ -2,17 +2,22 @@ require 'csv'
 
 
 class Card
-  attr_accessor :used
+  attr_accessor :used , :current_card
   attr_reader :definition, :answer,:id
   def initialize(args)
     @id = args[:id]
     @definition = args[:definition]
     @answer = args[:answer]
     @used = false
+    @current_card = nil
   end
 
   def has_been_used
     self.used = true
+  end
+
+  def skipped
+    self.used = false
   end
 
   def to_s
@@ -34,14 +39,18 @@ class Deck
     end
   end
 
+  def put_back_in_deck
+    current_card.skipped
+  end
+
   def change_card_status(card)
     card.has_been_used
     card
   end
 
   def new_card
-    card = contents.select{|card| card.used == false}.sample
-    change_card_status(card)
+    self.current_card = contents.select{|card| card.used == false}.sample
+    change_card_status(current_card)
   end
 
   def add_card(args)
