544e6b18977b8c4c1bbaa0478d0755e6a85b812b
Apprentice <apprentice@dbc-chicago-63.local>
2014-02-07 10:25:43 -0600

    pseudocode and initial wishful code

--- flashcards.rb
+++ flashcards.rb
@@ -1,1 +1,26 @@
-# Main file for 
+# Main file for
+
+class Controller
+
+  def initialize
+    @deck = Deck.new
+    @view = View.new
+    @input = ""
+  end
+
+  def run!
+    view.greet
+    view.give_definition
+    self.input = view.get_input
+  end
+
+end
+
+
+
+
+get input
+if they want to exit, exit
+  otherwise, check their answer
+  return true or false
+    tell them hooray or guess again
