e7dac79c968b043c81a2a9e3595b78d5b44daa61
Apprentice <apprentice@dbc-chicago-019.local>
2014-02-07 10:49:29 -0600

    tested cards

--- card.rb
+++ card.rb
@@ -1,20 +1,21 @@
 class Card
-  attr_reader :question, :reader,:id
+  attr_reader :question, :answer,:id, :used
   def initialize(args)
     @id = args[:id]
     @question = args[:question]
     @answer = args[:answer]
+    @used = false
   end
 end
 
 class Deck
-  attr_reader :csv_file
+  attr_reader :csv_file, :contents
   def initialize(csv_file)
     @csv_file = csv_file
     @contents = []
   end
 
-  def parse_file
+  def populate_contents
     csv_file.each{|args| contents << add_card(args)}
   end
 
@@ -23,11 +24,21 @@ class Deck
   end
 
   private
-  attr_accessor :contents
+  attr_writer :contents
 
 end
 
+# a bit of testing
+# questions = [ {question: "What is your name", answer: "Alex" ,id: "1" },
+#               {question: "What is your age", answer: "27" ,id: "2"},
+#               {question: "What color is an orange", answer: "orange" ,id: "3"}]
 
+# my_deck = Deck.new(questions)
+
+# my_deck.populate_contents
+
+# p my_deck.contents[1].question
+# p my_deck.contents[1].answer
 
 
 
